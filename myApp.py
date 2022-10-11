c328cf293cf257a85e98a1b90a3d8b59c35b4257
Srdjan Sumarac <sumaracsrdjan@gmail.com>
2022-10-11 02:10:13 -0400

    save

--- myApp.py
+++ myApp.py
@@ -681,8 +681,103 @@ def main():
     # st.sidebar.write(name10, val10)
     # st.sidebar.write(name11, val11)
 
-    text_contents = '''This is some text'''
-    st.sidebar.download_button('Download some text', text_contents)
+    with io.BytesIO() as buffer:
+        # Write array to buffer
+        np.save(buffer, raw_data)
+        btn = st.download_button(
+            label="Download numpy array (.npy)",
+            data = buffer, # Download buffer
+            file_name = 'predicted_map.npy'
+        ) 
+
+
+
+
+
+    def save_segment(self, events):
+        file = sp.SonFile(sName=self.read_filepath, bReadOnly=True)
+
+        read_max_time = file.ChannelMaxTime(self.channel) * file.GetTimeBase()
+
+        period = file.ChannelDivide(self.channel) * file.GetTimeBase()
+        num_points = math.floor(read_max_time / period)
+
+        time_points = np.arange(0, num_points * period, period)
+        wave = file.ReadInts(self.channel, num_points, 0)
+
+        if len(wave) == 0:
+            print('There was no data read.')
+            quit()
+        elif len(wave) != num_points:
+            print(f'Mismatched number of points. Expected {num_points} points, but got {len(wave)} instead.')
+            quit()
+
+        filename = self.save_filepath
+        new = sp.SonFile(filename)
+
+        wave_offset = file.GetChannelOffset(self.channel)
+        wave_scale = file.GetChannelScale(self.channel)
+        wave_time_base = file.GetTimeBase()
+        wave_Y_range = file.GetChannelYRange(self.channel)
+        wave_Y_low = wave_Y_range[0]
+        wave_Y_high = wave_Y_range[1]
+        wave_units = file.GetChannelUnits(self.channel)
+        wave_title = file.GetChannelTitle(self.channel)
+        wave_channel_type = file.ChannelType(self.channel)
+        wave_sample_ticks = file.ChannelDivide(self.channel)
+        wave_Fs = 1 / period
+        time_start = 0
+
+        new.SetTimeBase(wave_time_base)
+        new_wave_channel = 0
+
+        new.SetWaveChannel(new_wave_channel, wave_sample_ticks, sp.DataType.Adc, wave_Fs)
+        new.SetChannelTitle(new_wave_channel, wave_title)
+        new.SetChannelUnits(new_wave_channel, wave_units)
+        new.SetChannelScale(new_wave_channel, wave_scale)
+        new.SetChannelOffset(new_wave_channel, wave_offset)
+        new.SetChannelYRange(new_wave_channel, wave_Y_low, wave_Y_high)
+
+        spike_channel = new.GetFreeChannel()
+        max_event_rate = 1 / (wave_time_base * wave_sample_ticks)
+
+        new.SetEventChannel(spike_channel, max_event_rate, sp.DataType.EventFall)
+        new.SetChannelTitle(spike_channel, 'ST-1')
+        new.SetChannelUnits(spike_channel, wave_units)
+        new.SetChannelScale(spike_channel, wave_scale)
+        new.SetChannelOffset(spike_channel, wave_offset)
+        new.SetChannelYRange(spike_channel, wave_Y_low, wave_Y_high)
+        del new
+
+        reload = sp.SonFile(filename, False)
+        if self.segment_inverted:
+            wave = [-i for i in wave]
+        write_wave = reload.WriteInts(new_wave_channel, wave, time_start)
+
+        if write_wave < 0:
+            print(f'Error code: {write_wave}')
+            print(sp.GetErrorString(write_wave))
+        else:
+            print('Waveform written successfully.')
+
+        spike_ticks = [int(i / wave_time_base) for i in events]
+        spike_times_formatted = np.array(spike_ticks, dtype=np.int64)
+        write_train = reload.WriteEvents(spike_channel, spike_times_formatted)
+
+        if write_train < 0:
+            print(f'Error Code: {write_train}')
+            print(sp.GetErrorString(write_train))
+            status = -1
+        else:
+            print('Spike train written successfully.')
+            status = 0
+
+        del reload
+
+        return status
+
+
+
 
 st.title('TNBS Spooky Spikes Online')
 
