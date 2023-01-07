5e52fd954eb42111720f3693f89c2904c8b6d636
Srdjan Sumarac <sumaracsrdjan@gmail.com>
2023-01-07 22:05:14 +1100

    temp

--- spooky_spikes_online.py
+++ spooky_spikes_online.py
@@ -545,8 +545,8 @@ def main():
             st.table(psd_power_df)
             
             st.sidebar.title("Machine Learning")
-            st.sidebar.header("NOTE: ""Spiketrain Oscillation Analysis"" must be checked to enable ML predictions.")
-            dbs_target = st.selectbox('Select DBS Target', ['STN', 'GPi'])
+            st.sidebar.write("NOTE: ""Spiketrain Oscillation Analysis"" must be checked to enable ML predictions.")
+            dbs_target = st.sidebar.selectbox('Select DBS Target', ['STN', 'GPi'])
             
 
 
